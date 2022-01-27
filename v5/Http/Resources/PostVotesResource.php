e38db8324e4a6aa108afb5c6fc6ba17228461049
Peter Tandler <p.github@tandlers.de>
2022-01-27 15:13:27 +0100

    feat: vote for posts part 2
    - added relationship between Post and PostVotes
    - added `votes` to PostRessource
    - extended PostVotesResource to return aggregated votes (sum, avg. count)
      and optionally the full details

--- v5/Http/Resources/PostVotesResource.php
+++ v5/Http/Resources/PostVotesResource.php
@@ -1,12 +1,48 @@
 <?php
+
 namespace v5\Http\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
 
 /**
- * TODO:PET do I actually _need_ this class ...?
+ * The `PostVotesResource` returns the aggregated votes for the post (sum, avg. count)
+ * and optionally (i.e. when $include_details is true) the full details
  */
 class PostVotesResource extends Resource
 {
-    public static $wrap = 'result';
+    /**
+     * @var bool if true, the ressource will also return all the information about who votes with which value when
+     */
+    protected $include_details = false;
+
+    /**
+     * create a `PostVotesResource` that also returns all the details about the individual votes
+     *
+     * @param $resource
+     * @return PostVotesResource
+     */
+    public static function withDetails($resource): PostVotesResource
+    {
+        $instance = new self($resource);
+        $instance->include_details = true;
+        return $instance;
+    }
+
+    public function toArray($request): array
+    {
+        $arr = [
+            'sum' => $this->resource->sum('vote'),
+            'avg' => $this->resource->avg('vote'),
+            'count' => $this->resource->count(),
+        ];
+        if ($this->include_details) {
+/*
+            $arr['votes'] = $this->resource->map(function ($item) {
+                return $item->vote;
+            });
+*/
+            $arr['result'] = parent::toArray($request);
+        }
+        return $arr;
+    }
 }
